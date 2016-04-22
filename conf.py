3994c0b8cf838f7af2f2f21d578cd5457daa1b4a
Tomas Orsava <torsava@redhat.com>
2016-04-22 12:35:55 +0200

    Polished the copyright.

--- conf.py
+++ conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Python RPM Porting Guide'
-copyright = '2016, Red Hat, Inc.'
+copyright = '2016, Red Hat, Inc'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
