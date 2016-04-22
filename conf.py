874c62d4804c40c2dcd0886c302ab5259b2a92fe
Tomas Orsava <torsava@redhat.com>
2016-04-22 12:32:53 +0200

    Updated project metadata, changed copyright to Red Hat, Inc.

--- conf.py
+++ conf.py
@@ -46,8 +46,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'Python RPM Porting'
-copyright = '2016, Tomas Orsava'
+project = 'Python RPM Porting Guide'
+copyright = '2016, Red Hat, Inc.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -179,7 +179,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'PythonRPMPortingdoc'
+htmlhelp_basename = 'PythonRPMPortingGuide'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -199,8 +199,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'PythonRPMPorting.tex', 'Python RPM Porting Documentation',
-   'Tomas Orsava', 'manual'),
+  ('index', 'PythonRPMPortingGuide.tex', 'Python RPM Porting Guide',
+   'Red Hat, Inc.', 'howto'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -229,8 +229,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'pythonrpmporting', 'Python RPM Porting Documentation',
-     ['Tomas Orsava'], 1)
+    ('index', 'pythonrpmportingguide', 'Python RPM Porting Guide',
+     ['Red Hat, Inc.'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -243,8 +243,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'PythonRPMPorting', 'Python RPM Porting Documentation',
-   'Tomas Orsava', 'PythonRPMPorting', 'One line description of project.',
+  ('index', 'PythonRPMPortingGuide', 'Python RPM Porting Guide',
+   'Red Hat, Inc.', 'PythonRPMPortingGuide', 'Guide for porting Python 2 RPM packages to Python 3',
    'Miscellaneous'),
 ]
 
